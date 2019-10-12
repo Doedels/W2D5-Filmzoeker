064405de7b2f9f74fa8158cb987f4e75ca44b7a9
Doedels <sandermarkies@yahoo.com>
2019-10-12 13:39:52 +0200

    Toevoegen van vituele DOM en wat test

--- script.test.js
+++ script.test.js
@@ -1,14 +1,36 @@
+// dit testdocument heeft geen toegang tot de DOM.
+// er moet dus een DOM met relevante element gecreerd worden
+// bovenaan het document.
+document.body.innerHTML = '<div><button></button></div>';
 
-//let script = require('./script.js');
-//let data = require('./data.js') die data zorgt voor problemen ??
+let script = require('./script.js');
 
-// voorbeeld van een test
-test('filterMovies roept displayMovies aan', () => {
+// voorbeeld van 2 simpele tests zonder externe afhankelijkheid
+test('primitive types', () => {
+    let input = 3 * 3;
+    let output = 9;
+
+    expect(input).toBe(output);
+});
+
+test('array en objects', () => {
     let input = [1,2,3].map( e => e * 2);
     let output = [2,4,6];
 
     expect(input).toEqual(output);
 });
+
+// en dan nu de serieuze tests.
+
+test('GetUnique() returned een ontdubbelde Array met objecten', () => {
+    data = [{Title: 'X-Man'}, {Title: 'Avangers'}, {Title: 'Prinsess'}, {Title: 'Avangers'}]
+    let input = script.getUnique(data, 'Title');
+    let output = [{Title: 'X-Man'}, {Title: 'Avangers'}, {Title: 'Prinsess'}];
+
+    expect(input).toEqual(output);
+});
+
+
 
 
 // script.displayMovies(movieArray)
