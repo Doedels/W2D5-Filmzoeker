fec6fc01c46b735a4ec285fd0a7dcd149b8c4919
Doedels <sandermarkies@yahoo.com>
2019-10-13 20:03:23 +0200

    Toevoegen testgevallen

--- script.test.js
+++ script.test.js
@@ -1,7 +1,7 @@
 // dit testdocument heeft geen toegang tot de DOM.
 // er moet dus een DOM met relevante element gecreerd worden
 // bovenaan het document.
-document.body.innerHTML = '<div><button></button></div>';
+document.body.innerHTML = '<div id="posters"><button></button></div>';
 
 let script = require('./script.js');
 
@@ -14,24 +14,98 @@ test('primitive types', () => {
 });
 
 test('array en objects', () => {
-    let input = [1,2,3].map( e => e * 2);
-    let output = [2,4,6];
+    let input = [1, 2, 3].map(e => e * 2);
+    let output = [2, 4, 6];
 
     expect(input).toEqual(output);
 });
 
 // en dan nu de serieuze tests.
+movies = [
+    {
+        "Title": "X-Man",
+        "Year": "2017",
+        "imdbID": "X-Man"
+    },
+    {
+        "Title": "Avangers",
+        "Year": "2014",
+        "imdbID": "Avangers"
+    },
+    {
+        "Title": "Prinsess",
+        "Year": "2013",
+        "imdbID": "Prinsess"
+    },
+    {
+        "Title": "Batman",
+        "Year": "1999",
+        "imdbID": "Batman"
+    }
+]
+
+let dubbel = movies.push({
+    "Title": "Avangers",
+    "Year": "2014",
+    "imdbID": "Avangers"
+})
 
 test('GetUnique() returned een ontdubbelde Array met objecten', () => {
-    data = [{Title: 'X-Man'}, {Title: 'Avangers'}, {Title: 'Prinsess'}, {Title: 'Avangers'}]
-    let input = script.getUnique(data, 'Title');
-    let output = [{Title: 'X-Man'}, {Title: 'Avangers'}, {Title: 'Prinsess'}];
+    let input = script.getUnique(dubbel, 'Title');
+    let output = movies;
 
     expect(input).toEqual(output);
 });
 
+test('filterMovies moet een subArray met alleen bepaalde titels teruggeven', () => {
+    //const script.displayMovies = jest.fn();
+    let input = script.filterMovies(movies, 'Nieuwste');
+    let output = movies.slice(0, 2);
 
+    expect(input).toEqual(output);
+});
 
+test('filterMovies moet een subArray met alleen bepaalde titels teruggeven', () => {
+    //const script.displayMovies = jest.fn();
+    let input = script.filterMovies(movies, 'X-Man');
+    let output = movies.slice(0, 1);
 
-// script.displayMovies(movieArray)
-// script.filterMovies(radioButtonId)
+    expect(input).toEqual(output);
+});
+
+test('filterMovies moet een subArray met alleen bepaalde titels teruggeven', () => {
+    //const script.displayMovies = jest.fn();
+    let input = script.filterMovies(movies, 'Avangers');
+    let output = movies.slice(1, 2);
+
+    expect(input).toEqual(output);
+});
+
+test('filterMovies moet een subArray met alleen bepaalde titels teruggeven', () => {
+    //const script.displayMovies = jest.fn();
+    let input = script.filterMovies(movies, 'Prinsess');
+    let output = movies.slice(2, 3);
+
+    expect(input).toEqual(output);
+});
+
+test('filterMovies moet een subArray met alleen bepaalde titels teruggeven', () => {
+    //const script.displayMovies = jest.fn();
+    let input = script.filterMovies(movies, 'Batman');
+    let output = movies.slice(3, 4);
+
+    expect(input).toEqual(output);
+});
+
+test('getMovieString moet een <a><img></a> string teruggeven', () => {
+    //const script.displayMovies = jest.fn();
+    let input = script.getMovieString(movies);
+
+    expect(input).toMatch(/<a href="https:/);
+    expect(input).toMatch(/www.imdb.com/);
+    expect(input).toMatch(/" target="_blank"><img src="/);
+    expect(input).toMatch(/X-Man/);
+    expect(input).toMatch(/Avangers/);
+    expect(input).toMatch(/Prinsess/);
+    expect(input).toMatch(/Batman/);
+});
